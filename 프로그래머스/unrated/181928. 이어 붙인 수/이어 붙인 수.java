522e261618602c85759d14b79641c6656c920623
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2023-05-31 19:19:38 +0900

    [unrated] Title: 이어 붙인 수, Time: 10.22 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        
+        for(int i : num_list) {
+            if(i % 2 == 0) {
+                even += i;
+            } else {
+                odd += i;
+            }
+        }
+        answer = Integer.valueOf(odd) + Integer.valueOf(even);
+        
+        return answer;
+    }
+}
