08d4c4423a703046a655c350c3d7769d4e3eb320
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2023-06-13 22:15:09 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 5.17 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        char[] c = new char[my_string.length()];
+        
+        char c1 = ' ';
+        char c2 = ' ';
+        char temp = ' ';
+        
+        for(int i = 0; i < my_string.length(); i++) {
+            c[i] = my_string.charAt(i);
+        }
+        
+        temp = c[num1];
+        c[num1] = c[num2];
+        c[num2] = temp;
+        
+        for(int i = 0; i < c.length; i++) {
+            answer += String.valueOf(c[i]);
+        }
+        
+        
+        return answer;
+    }
+}
