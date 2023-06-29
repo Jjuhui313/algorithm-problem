08ba658d0a31ab2910a8e4da809fbf1f262a3edf
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2023-06-29 23:45:33 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int count = 0;
+        int index = 0;
+        while(count < k) {
+            count++;
+            
+            if(index >= numbers.length) {
+                index -= numbers.length;
+            }
+            answer = numbers[index];
+            index += 2;
+            
+            
+            
+        }
+        
+        return answer;
+    }
+}
