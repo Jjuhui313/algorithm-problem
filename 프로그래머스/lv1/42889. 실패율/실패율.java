4b1756be565f3efcf001b0e5ac85ab8782e62949
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2023-07-19 23:52:09 +0900

    [level 1] Title: 실패율, Time: 0.02 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int noclear[] = new int[N];
+        double[] fail = new double[N];
+
+        for(int i : stages){
+            if(i != N + 1) {
+                noclear[i - 1] += 1;
+            }
+        }        
+
+        int player = stages.length;
+        for(int i = 0; i < N; i++){
+            fail[i]= (double)noclear[i] / player;
+            player -= noclear[i];
+            answer[i] = i + 1;
+        }
+        
+        double tmpFail = 0;
+        int tmpAnswer = 0;
+
+        for(int i = 0; i < N; i++){
+            for (int j = 1; j < N - i; j++) {
+                if(fail[j - 1] < fail[j]){
+                    tmpFail = fail[j - 1];
+                    fail[j - 1] = fail[j];
+                    fail[j] = tmpFail;
+                    
+                    tmpAnswer = answer[j-1];
+                    answer[j - 1] = answer[j];
+                    answer[j] = tmpAnswer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
