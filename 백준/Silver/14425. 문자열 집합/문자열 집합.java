092bfd3cdab5751b474d8959ef715eb4c05bb70a
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2024-01-07 22:22:10 +0900

    [Silver IV] Title: 문자열 집합, Time: 420 ms, Memory: 31136 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,7 +12,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int count = 0;
-        List<String> arrN = new ArrayList<>();
+        Set<String> arrN = new HashSet<>();
         for(int i = 0; i < N; i++) {
             arrN.add(br.readLine());
         }
