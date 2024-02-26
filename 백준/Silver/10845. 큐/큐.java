46c2cbdb7fdc8d5ca8b0233c1e6d3f5ddc256b2c
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2024-02-26 21:18:53 +0900

    [Silver IV] Title: 큐, Time: 196 ms, Memory: 19220 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int num = 0;
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            switch (st.nextToken()) {
+                case "push":
+                    num = Integer.parseInt(st.nextToken());
+                    queue.offer(num);
+                    break;
+                case "pop":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.poll()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(queue.peek()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (queue.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(num).append("\n");
+                    }
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
