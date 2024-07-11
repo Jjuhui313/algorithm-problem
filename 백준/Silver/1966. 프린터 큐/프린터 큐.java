7c76f514a4391abb963cff6fb2e673af01383244
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2024-07-11 11:34:01 +0900

    [Silver III] Title: 프린터 큐, Time: 132 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,9 +1,7 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,49 +9,47 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
         StringTokenizer st;
         while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
             LinkedList<int[]> q = new LinkedList<>();
+
             st = new StringTokenizer(br.readLine());
-
             for (int i = 0; i < N; i++) {
-
                 q.offer(new int[] {i, Integer.parseInt(st.nextToken())});
-
             }
 
-            int count = 0;
+            int cnt = 0;
 
             while (!q.isEmpty()) {
                 int[] front = q.poll();
                 boolean isMax = true;
-                for(int i = 0; i < q.size(); i++) {
-                    if(front[1] < q.get(i)[1]) {
 
+                for (int i = 0; i < q.size(); i++) {
+                    if (front[1] < q.get(i)[1]) {
                         q.offer(front);
-                        for(int j = 0; j < i; j++) {
-                            q.offer(q.poll());
-                        }
+                        for (int j = 0; j < i; j++) q.offer(q.poll());
 
                         isMax = false;
                         break;
                     }
                 }
-                if(isMax == false) {
+                if (!isMax) {
                     continue;
                 }
-                count++;
-                if(front[0] == M) {
+
+                cnt++;
+                if (front[0] == M) {
                     break;
                 }
             }
-
-            sb.append(count).append("\n");
+            sb.append(cnt).append("\n");
 
         }
+
         System.out.println(sb);
     }
 }
