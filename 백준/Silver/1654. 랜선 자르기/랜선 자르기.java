87af8afa71ecc99b8a23319e4c9667d1e48d69f5
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2024-07-31 22:42:31 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17428 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int[] line = new int[K];
+
+        for (int i = 0; i < K; i++) {
+            line[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(line);
+
+        long answer = 0L;
+        long left = 1L;
+        long right = line[line.length - 1];
+
+        while (left <= right) {
+            long sum = 0L;
+            long mid = (left + right) / 2;
+
+            for (long l : line) {
+                sum += l / mid;
+            }
+
+            if (sum < N) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+                answer = mid;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
