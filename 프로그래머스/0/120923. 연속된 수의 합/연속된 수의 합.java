ebe07bbfd754ff4e14f16462e0bfd9718a68439a
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-01-16 23:48:00 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int s = (total / num) - ((num - 1) / 2);
+        
+        for(int i = 0; i < num; i++) {
+            total -= s;
+            answer[i] = s;
+            s++;
+        }
+        
+        return answer;
+    }
+}
