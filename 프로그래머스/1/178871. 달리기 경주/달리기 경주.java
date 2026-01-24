e86581796e8cb891fb41a8db13480749feb6785d
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-01-24 17:08:38 +0900

    [level 1] Title: 달리기 경주, Time: 314.98 ms, Memory: 361 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -1,26 +1,28 @@
-import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        Map<String, Integer> hm = new HashMap<>();
-        
-        for (int i = 0; i < players.length; i++) {
-            hm.put(players[i], i);
+        String[] answer = players.clone();
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++){
+            map.put(players[i], i);
         }
-        for (String calling : callings) {
-            int idx = hm.get(calling).intValue();
-            
-            String prevPlayer = players[idx - 1];
-            String currentPlayer = players[idx];
-                
-            players[idx] = prevPlayer;
-            players[idx - 1] = currentPlayer;
-                
-            hm.put(currentPlayer, idx - 1);
-            hm.put(prevPlayer, idx);
-            
+
+        int tempN;
+        String tempS;
+        for (int i = 0; i < callings.length; i++){
+            tempN = map.get(callings[i]);
+
+            map.put(answer[tempN], map.get(answer[tempN]) - 1);
+            map.put(answer[tempN - 1], map.get(answer[tempN - 1]) + 1);
+
+            tempS = answer[tempN];
+            answer[tempN] = answer[tempN - 1];
+            answer[tempN - 1] = tempS;
         }
-        return players;
+
+        return answer;
     }
 }
