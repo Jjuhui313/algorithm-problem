346a3a11d4cd16b4600358889a4281b2f8850aa4
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-01-30 20:40:40 +0900

    [level 2] Title: [1차] 캐시, Time: 33.90 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Deque;
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        
+        Deque<String> cache = new LinkedList<>();
+
+        for (String city : cities) {
+            city = city.toLowerCase();
+
+            if (cache.contains(city)) {
+                cache.remove(city);
+                cache.addLast(city);
+                answer += 1;
+            } else {
+                if (cache.size() == cacheSize) {
+                    cache.pollFirst();
+                }
+                cache.addLast(city);
+                answer += 5;
+            }
+        }
+        
+        return answer;
+    }
+}
