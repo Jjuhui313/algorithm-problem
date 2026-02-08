56e47a566c676d383a78dfed1ddedc48e3edf371
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-02-08 23:39:28 +0900

    [level 0] Title: 5명씩, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
+        int j = 0;
+        for(int i = 0; i < names.length; i++) {
+            if(i % 5 == 0) {
+                answer[j] = names[i];
+                j++;
+            }
+        }
+        
+        return answer;
+    }
+}
