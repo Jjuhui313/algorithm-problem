13b3a20ed0c67617f25d956011051413cbf3e755
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-02-26 22:01:18 +0900

    [level 2] Title: 더 맵게, Time: 1702.00 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.add(s);
+        }
+        
+        while (pq.size() >= 2 && pq.peek() < K) {
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            
+            int mixed = first + (second * 2);
+            
+            pq.add(mixed);
+            answer++;
+        }
+        
+        if (pq.peek() >= K) {
+            return answer;
+        }
+        return -1;
+    }
+}
