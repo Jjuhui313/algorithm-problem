42c87d5c875d93486b59815b1560847183c16a0f
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-03-08 23:22:42 +0900

    [level 2] Title: 카펫, Time: 7.66 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,6 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-        /*
-            brown + yellow == answer[0] * answer[1]
-            brown + yellow = 12 = total
-            12 / 2 = 6
-            i == 가로
-            
-        */
         int total = brown + yellow;
         for (int i = 3; i <= total / 2; i++) {
             if (total % i != 0) {
