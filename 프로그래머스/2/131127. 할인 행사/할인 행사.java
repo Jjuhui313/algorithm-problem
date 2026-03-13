688cbdf2737b696155c2ce20da68d16a583a236b
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-03-13 22:23:31 +0900

    [level 2] Title: 할인 행사, Time: 41.88 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,42 +1,27 @@
+import java.util.ArrayList;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        int n = want.length;
-        int[] cur = new int[n];
+        ArrayList<String> arr = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
-            int idx = findIndex(want, discount[i]);
-            if (idx != -1) cur[idx]++;
+        for (int i = 0; i < want.length; i++) {
+            while (number[i]-- > 0) {
+                arr.add(want[i]);
+            }
         }
 
-        for (int start = 0; start <= discount.length - 10; start++) {
-            boolean ok = true;
-
-            for (int i = 0; i < n; i++) {
-                if (cur[i] < number[i]) {
-                    ok = false;
+        for (int i = 0; i < discount.length - arr.size() + 1;i++) {
+            ArrayList<String> list = (ArrayList<String>) arr.clone();
+            for (int j = i; j < i + arr.size(); j++) {
+                if (list.contains(discount[j])) {
+                    list.remove(discount[j]);
+                } else {
                     break;
                 }
             }
-
-            if (ok) answer++;
-
-            if (start + 10 < discount.length) {
-                int outIdx = findIndex(want, discount[start]);
-                if (outIdx != -1) cur[outIdx]--;
-
-                int inIdx = findIndex(want, discount[start + 10]);
-                if (inIdx != -1) cur[inIdx]++;
-            }
+            answer += list.size() == 0 ? 1 : 0;
         }
 
         return answer;
-    }
-    
-    private int findIndex(String[] want, String item) {
-        for (int i = 0; i < want.length; i++) {
-            if (want[i].equals(item)) return i;
-        }
-        return -1;
     }
 }
