50bd3d6e6fe0f29e6304485fdb54facbc874ecbc
김주희 <94800969+Jjuhui313@users.noreply.github.com>
2026-03-30 23:21:16 +0900

    [level 1] Title: 과일 장수, Time: 108.33 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,22 +3,11 @@ import java.util.Arrays;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        int[] arr = new int[score.length / m];
-        int index = 0;
-        int min = 10;
-        
+
         Arrays.sort(score);
-        
-        for (int i = score.length - 1; i >= m - 1; i -= m) {
-            for (int j = i; j > i - m; j--) {
-                min = Math.min(min, score[j]);
-            }
-            
-            arr[index++] = min * m;
-        }
-        
-        for(int i : arr) {
-            answer += i;
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
 
         return answer;
